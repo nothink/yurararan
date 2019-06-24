e364519d4ba455eb706e90b7d128547176281a8c
nothink <nothing@yasagure.jp>
2019-06-24 21:50:45 +0900

    Fix a bug (not sending mail)

--- src/handler/api.go
+++ src/handler/api.go
@@ -80,4 +80,9 @@ func sendUpdateMail(s []interface{}) {
 		/* To */ "nothink@nothink.jp",
 	)
 	msg.SetHtml("<html>HTML version of the body</html>")
+
+	msg, id, err := mg.Send(message)
+	if err != nil {
+		log.Fatalf("Could not send message: %v, ID %v, %+v", err, id, msg)
+	}
 }
